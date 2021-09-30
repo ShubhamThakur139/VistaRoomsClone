d7a1620f13f9bb9a709dd6dd21bd66e1b7612cea
Shubham Thakur <shubhamthakur2920@gmail.com>
2021-09-30 12:52:39 +0530

    adding functionalities

--- Js/CityPage.js
+++ Js/CityPage.js
@@ -6,6 +6,10 @@ function appendData(data) {
   data.forEach((element) => {
     let col = document.createElement("div");
     col.setAttribute("class", "col");
+    col.addEventListener("click", () => {
+      localStorage.setItem("villaInfo", JSON.stringify(element));
+      location.href = "../Assets/VillaDetails.html";
+    });
     col.innerHTML = `
       <div class="card">
         <img
@@ -39,7 +43,6 @@ function appendData(data) {
 async function getData() {
   let data = await fetch("http://localhost:3000/villas");
   let res = await data.json();
-  //   console.log("res:", res);
   appendData(res);
 }
 
